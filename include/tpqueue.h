503bb4ff0b9b15811f0edb03354c172f5afcdfd9
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-06-10 19:25:39 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,8 +4,71 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ public:
+    TPQueue(): head(nullptr), tail(nullptr) {}
+    void push(const T&);
+    T pop();
+ private:
+    struct ITEM {
+      T value;
+      ITEM *next;
+      ITEM *prew;
+    };
+    TPQueue:: ITEM *creates(const T&);
+    ITEM *head;
+    ITEM *tail;
 };
+
+template<typename T>
+typename TPQueue<T>::ITEM *TPQueue<T>::creates(const T& value) {
+  ITEM * temp = new ITEM;
+  temp->value = value;
+  temp->next = nullptr;
+  temp->prew = nullptr;
+  return temp;
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+  if (head && tail) {
+    ITEM* temp = head->next;
+    if (temp) {
+      temp->prew = nullptr;
+    }
+    T result = head->value;
+    delete head;
+    head = temp;
+    if (!head) {
+      tail = nullptr;
+    }
+    return result;
+  }
+}
+
+template<typename T>
+void TPQueue <T>::push(const T& data) {
+  ITEM *temp = head;
+  ITEM* item = creates(data);
+  while (temp && temp->value.prior >= data.prior) {
+    temp = temp->next;
+  }
+  if (!temp && head) {
+    tail ->next = item;
+    item->prew = tail;
+    tail = item;
+  } else if (!temp && !head) {
+    head = tail = item;
+  } else if (!temp->prew) {
+    head->prew = item;
+    item->next = head;
+    head = item;
+  } else {
+    temp->prew->next = item;
+    item->prew = temp->prew;
+    item->next = temp;
+    temp->prew = item;
+  }
+}
 
 struct SYM {
   char ch;
